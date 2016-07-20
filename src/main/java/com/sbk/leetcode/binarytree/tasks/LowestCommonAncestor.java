7b42e31486fc06ce0a3a3651fdb481efc13610a6
SergiySobolev <sergiy.sobolev@gmail.com>
2016-07-20 21:37:27 +0300

    better solution LCA

--- src/main/java/com/sbk/leetcode/binarytree/tasks/LowestCommonAncestor.java
+++ src/main/java/com/sbk/leetcode/binarytree/tasks/LowestCommonAncestor.java
@@ -5,17 +5,12 @@ import com.sbk.leetcode.binarytree.TreeNode;
 public class LowestCommonAncestor {
 
     public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
-        if(canBeReached(root.left, p) && canBeReached(root.left, q))
-            return lowestCommonAncestor(root.left, p, q);
-        if(canBeReached(root.right, p) && canBeReached(root.right, q))
-            return lowestCommonAncestor(root.right, p, q);
-        return root;
-    }
-
-    public boolean canBeReached(TreeNode from, TreeNode to){
-        if(from == null) return false;
-        if(from == to) return true;
-        return canBeReached(from.left, to) || canBeReached(from.right, to);
+        if (root == p || root == q || root == null) { return root; }
+        TreeNode left = lowestCommonAncestor(root.left, p, q);
+        TreeNode right = lowestCommonAncestor(root.right, p, q);
+        return (left != null && right != null)
+                ? root
+                : (left != null ? left : right);
     }
 
 }
