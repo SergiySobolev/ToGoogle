a52e50fd9b604337d7c38630cfc1346419896562
SergiySobolev <sergiy.sobolev@gmail.com>
2016-07-20 20:51:47 +0300

    recursive house robber 3

--- src/main/java/com/sbk/leetcode/binarytree/tasks/HouseRobber.java
+++ src/main/java/com/sbk/leetcode/binarytree/tasks/HouseRobber.java
@@ -1,47 +1,22 @@
 package com.sbk.leetcode.binarytree.tasks;
 
-import com.google.common.collect.Lists;
 import com.sbk.leetcode.binarytree.TreeNode;
 
-import java.util.*;
+import static java.lang.Math.max;
 
 public class HouseRobber {
-    public int rob(TreeNode treeNode) {
-        if(treeNode == null) {
-            return 0;
-        }
-        List<Integer> levelsSum = new ArrayList<>();
-        Queue<TreeNode> queue = new ArrayDeque<>();
-        queue.add(treeNode);
-        int queueSizeOnCurrentLevel = queue.size();
-        while(queue.size() > 0) {
-            int currentSum = 0;
-            for (int i = 0; i < queueSizeOnCurrentLevel; i++) {
-                TreeNode currentNode = queue.poll();
-                currentSum += currentNode.val;
-                if(Objects.nonNull(currentNode.left)) queue.add(currentNode.left);
-                if(Objects.nonNull(currentNode.right)) queue.add(currentNode.right);
-            }
-            System.out.println();
-            queueSizeOnCurrentLevel = queue.size();
-            levelsSum.add(currentSum);
-        }
-        return maxSubSequenceSum(levelsSum);
-    }
+    public int rob(TreeNode root) {
+        int[] res = dfs(root);
+        return max(res[0], res[1]);
 
-    public int maxSubSequenceSum(List<Integer> levelsSum) {
-        if(levelsSum.size() == 0){
-            return 0;
-        }
-        if(levelsSum.size() == 1) {
-            return levelsSum.get(0);
-        }
-        if(levelsSum.size() == 2) {
-            return Math.max(levelsSum.get(0), levelsSum.get(1));
-        }
-        return Math.max(
-                maxSubSequenceSum(levelsSum.subList(1, levelsSum.size())),
-                levelsSum.get(0) + maxSubSequenceSum(levelsSum.subList(2, levelsSum.size()))
-                );
+    }
+    public int[] dfs(TreeNode root){
+        if(root == null) return new int[2];
+        int[] right =  dfs(root.right);
+        int[] left =  dfs(root.left);
+        int[] result = new int[2];
+        result[0] = root.val + right[1] + left[1];
+        result[1] = max(left[0], left[1]) + max(right[0], right[1]);
+        return result;
     }
 }
